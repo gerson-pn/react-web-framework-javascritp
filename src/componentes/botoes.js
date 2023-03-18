53fb0dfd89bc00a5fde13835e0dcdfab913d0098
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2023-03-18 15:17:17 -0300

    Correção nos atributos das tags, troca de class para className

--- src/componentes/botoes.js
+++ src/componentes/botoes.js
@@ -4,10 +4,10 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 const Botoes = () => {
     return (
         <div className="container">
-            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                <button type="button" class="btn btn-danger">Left</button>
-                <button type="button" class="btn btn-warning">Middle</button>
-                <button type="button" class="btn btn-success">Right</button>
+            <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                <button type="button" className="btn btn-danger">Left</button>
+                <button type="button" className="btn btn-warning">Middle</button>
+                <button type="button" className="btn btn-success">Right</button>
             </div>
         </div>
     )
--- src/pages/login.js
+++ src/pages/login.js
@@ -6,40 +6,40 @@ export default function Login() {
             <form>
 
                 <legend>Exemplo de formulário</legend>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">@</span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">@</span>
+                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
                 </div>
 
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                    <span class="input-group-text" id="basic-addon2">@example.com</span>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                    <span className="input-group-text" id="basic-addon2">@example.com</span>
                 </div>
 
-                <div class="mb-3">
-                    <label for="basic-url" class="form-label">Your vanity URL</label>
-                    <div class="input-group">
-                        <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" />
+                <div className="mb-3">
+                    <label for="basic-url" className="form-label">Your vanity URL</label>
+                    <div className="input-group">
+                        <span className="input-group-text" id="basic-addon3">https://example.com/users/</span>
+                        <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
                     </div>
-                    <div class="form-text">Example help text goes outside the input group.</div>
+                    <div className="form-text">Example help text goes outside the input group.</div>
                 </div>
 
-                <div class="input-group mb-3">
-                    <span class="input-group-text">$</span>
-                    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" />
-                    <span class="input-group-text">.00</span>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">$</span>
+                    <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
+                    <span className="input-group-text">.00</span>
                 </div>
 
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" />
-                    <span class="input-group-text">@</span>
-                    <input type="text" class="form-control" placeholder="Server" aria-label="Server" />
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" />
+                    <span className="input-group-text">@</span>
+                    <input type="text" className="form-control" placeholder="Server" aria-label="Server" />
                 </div>
 
-                <div class="input-group">
-                    <span class="input-group-text">With textarea</span>
-                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                <div className="input-group">
+                    <span className="input-group-text">With textarea</span>
+                    <textarea className="form-control" aria-label="With textarea"></textarea>
                 </div>
                 <br />
                 <a href="/home" type="btn" className="btn btn-primary">Enviar</a>
